6ca3ea34e681a6314346ddf1600298e7ad91d98f
Proga147 <Proga147@users.noreply.github.com>
2017-04-01 10:51:01 -0400

    Uniform distribution

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,11 +1,32 @@
 #include "ofApp.h"
+#include <random>
+#include <vector>
+
 
 //--------------------------------------------------------------
 void ofApp::setup(){
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+
+int xmin=0; int xmax=1200;
+int ymin=0; int ymax=970;
+
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution< > Xdis(xmin,xmax);
+std::uniform_int_distribution< > Ydis(ymin,ymax);
+
+for(int i =0; i<20; i++) {
+X.push_back(Xdis(gen));
+Y.push_back(Ydis(gen));
 }
+
+
+
+
+}
+
 
 //--------------------------------------------------------------
 void ofApp::update(){
